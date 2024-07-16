dfac71ca34fc33e0ec49787270ee3a61b40c43e8
minhttran <trcongminh.261002@gmail.com>
2024-07-16 22:38:59 +0700

    Update path

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,13 +1,12 @@
 'use client'
 
 import { Input } from '../ui/input'
-import { useSearchParams, usePathname, useRouter } from 'next/navigation'
+import { useSearchParams, useRouter } from 'next/navigation'
 import { useDebouncedCallback } from 'use-debounce'
 import { useState, useEffect } from 'react'
 
 const NavSearch = () => {
     const searchParams = useSearchParams();
-    const pathName = usePathname();
     const { replace } = useRouter();
 
     const [search, setSearch] = useState(searchParams.get('search')?.toString() || '');
@@ -20,7 +19,7 @@ const NavSearch = () => {
             params.delete('search')
         }
 
-        replace(`${pathName}?${params.toString()}`);
+        replace(`/?${params.toString()}`);
     }, 500);
 
     useEffect(() => {
