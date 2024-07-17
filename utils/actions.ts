106215d8f8992407545b3e86ec414d1c9fb1ab96
minhttran <trcongminh.261002@gmail.com>
2024-07-17 22:25:59 +0700

    Reservations

--- utils/actions.ts
+++ utils/actions.ts
@@ -599,3 +599,31 @@ export const updatePropertyImageAction = async (
         return renderError(error);
     }
 };
+
+export const fetchReservations = async () => {
+    const user = await getAuthUser();
+  
+    const reservations = await db.booking.findMany({
+      where: {
+        property: {
+          profileId: user.id,
+        },
+      },
+  
+      orderBy: {
+        createdAt: 'desc', 
+      },
+  
+      include: {
+        property: {
+          select: {
+            id: true,
+            name: true,
+            price: true,
+            country: true,
+          },
+        }, 
+      },
+    });
+    return reservations;
+  };
--- utils/links.ts
+++ utils/links.ts
@@ -4,11 +4,12 @@ type NavLink = {
 }
 
 export const links: NavLink[] = [
-    { href: "/", label: "Home" },
-    { href: "/favorites", label: "Favorites" },
-    { href: "/bookings", label: "Bookings" },
-    { href: "/reviews", label: "reviews" },
-    { href: "/rentals/create", label: "Add Rental" },
-    { href: "/rentals", label: "Rentals" },
-    { href: "/profile", label: "Profile" },
+    { href: '/', label: 'home' },
+    { href: '/favorites ', label: 'favorites' },
+    { href: '/bookings ', label: 'bookings' },
+    { href: '/reviews ', label: 'reviews' },
+    { href: '/reservations ', label: 'reservations' },
+    { href: '/rentals/create ', label: 'create rental' },
+    { href: '/rentals', label: 'my rentals' },
+    { href: '/profile ', label: 'profile' },
 ];
