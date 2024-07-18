21840c1d64a33ad0066b9fb84542dff41d196d32
minhttran <trcongminh.261002@gmail.com>
2024-07-18 11:12:33 +0700

    Vercel analytic

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Providers from "./providers";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
+      <Analytics />
       <html lang="en" suppressHydrationWarning>
         <body className={inter.className}>
           <Providers>
