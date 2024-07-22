ca246f0ff5347c7bed18b2562aca3ac6c8b9571d
minhttran <trcongminh.261002@gmail.com>
2024-07-22 08:40:22 +0700

    Update deployment

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import Link from 'next/link'
-import { Button } from '../ui/button'
-import Image from 'next/image'
+import Link from 'next/link';
+import Image from 'next/image';
 
-const Logo = () => {
+export default function Header() {
     return (
-        <Link href='/'>
-            <Image src='/logo.png' alt='logo' width={130} height={130} />
-        </Link>
-    )
+        <>
+            <head>
+                <link rel="preload" href="/logo.png" as="image" />
+            </head>
+            <Link href='/'>
+                <Image
+                    src='/logo.png'
+                    alt='logo'
+                    width={130}
+                    height={130}
+                    priority
+                    decoding="async"
+                />
+            </Link>
+        </>
+    );
 }
-
-export default Logo
